7b65f202d447859bc343df1b4841b21ce0676c9b
Kosuke Maeda <maeda.korosuke@gmail.com>
2018-07-04 19:13:47 +0900

    Initial commit.

---
+++ app/src/main/java/com/example/kosuke/gesture/AsyncHttp.java
@@ -0,0 +1,127 @@
+package com.example.kosuke.gesture;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Random;
+
+public class AsyncHttp extends AsyncTask<HashMap<String, String>, Void, JSONObject> {
+    HttpURLConnection con = null;
+    String rootUrl = "https://api-us.faceplusplus.com/humanbodypp/beta/gesture";
+
+    private CallBackTask callBackTask;
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    /**
+     * @param params request, json, access_token
+     */
+    @Override
+    protected JSONObject doInBackground(HashMap<String, String>... params) {
+        JSONObject retJson = null;
+        int status = -1;
+
+        String boundaryString = getBoundary();
+
+        try {
+            URL url = new URL(rootUrl);
+            con = (HttpURLConnection) url.openConnection();
+            con.setUseCaches(false);
+            con.setRequestMethod("POST");
+            con.setInstanceFollowRedirects(false);
+            con.setRequestProperty("accept", "*/*");
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+            con.setRequestProperty("connection", "Keep-Alive");
+            con.setRequestProperty("user-agent", "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1)");
+            con.setDoOutput(true);
+
+            DataOutputStream obos = new DataOutputStream(con.getOutputStream());
+            for (String key : params[0].keySet()) {
+                obos.writeBytes("--" + boundaryString + "\r\n");
+                obos.writeBytes("Content-Disposition: form-data; name=\"" + key
+                        + "\"\r\n");
+                obos.writeBytes("\r\n");
+                obos.writeBytes(params[0].get(key) + "\r\n");
+            }
+            obos.writeBytes("--" + boundaryString + "--" + "\r\n");
+            obos.writeBytes("\r\n");
+            obos.flush();
+            obos.close();
+
+            status = con.getResponseCode();
+            Log.d("status in AsyncHttp", String.valueOf(status));
+
+            final InputStream stream = (status == 200) ? con.getInputStream() : con.getErrorStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+            String buffer = reader.readLine();
+            retJson = new JSONObject(buffer);
+            retJson.put("status", status);
+
+            con.disconnect();
+        } catch (ConnectException e) {
+            e.printStackTrace();
+            return null;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return retJson;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject json) {
+        super.onPostExecute(json);
+        if (callBackTask != null) {
+            callBackTask.CallBack(json);
+        }
+    }
+
+    public void setOnCallBack(CallBackTask _cbj) {
+        callBackTask = _cbj;
+    }
+
+    public static interface CallBackTask {
+        public void CallBack(JSONObject json);
+    }
+
+    private static String getBoundary() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 32; ++i) {
+            sb.append("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-".charAt(random.nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_".length())));
+        }
+        return sb.toString();
+    }
+
+    private static String encode(String value) throws Exception {
+        return URLEncoder.encode(value, "UTF-8");
+    }
+
+}
---
+++ app/src/main/java/com/example/kosuke/gesture/MainActivity.java
@@ -0,0 +1,237 @@
+package com.example.kosuke.gesture;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private Camera mCamera;
+    private TextureView mTexutreView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTexutreView = findViewById(R.id.texture);
+        mTexutreView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                mCamera.open();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
+
+        mCamera = new Camera(this, mTexutreView);
+        findViewById(R.id.button_capture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = mCamera.capture();
+                requestApi(bitmap);
+            }
+        });
+
+    }
+
+    public void requestApi(Bitmap bitmap) {
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("api_key", getString(R.string.api_key));
+        params.put("api_secret", getString(R.string.api_secret_key));
+        params.put("image_base64", convert2Base64(bitmap));
+
+        Log.d("MainActivity#request", "request start.");
+        AsyncHttp task = new AsyncHttp();
+        task.setOnCallBack(new AsyncHttp.CallBackTask() {
+            @Override
+            public void CallBack(JSONObject json) {
+                Log.d("returned value", json.toString());
+            }
+        });
+        task.execute(params);
+
+    }
+
+    public static String convert2Base64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        return Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+    }
+}
+
+
+class Camera {
+    private static int REQUEST_CODE_CAMERA_PERMISSION = 100;
+
+    private CameraDevice mCamera;
+    private TextureView mTextureView;
+    private Size mCameraSize;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private CameraCaptureSession mPreviewSession;
+    private Context mContext;
+
+    private CameraDevice.StateCallback mCameraDeviceCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCamera = camera;
+            createCaptureSession();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+            mCamera = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            mCamera = null;
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mCameraCaptureSessionCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            mPreviewSession = session;
+            updatePreview();
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+            Toast.makeText(mContext, "onConfigureFailed", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    public Camera(Context context, TextureView textureView) {
+        this.mContext = context;
+        this.mTextureView = textureView;
+    }
+
+    public void open() {
+        try {
+            CameraManager manager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                if (characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
+                    StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    mCameraSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                    if (PermissionChecker.checkSelfPermission(mContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        requestCameraPermission();
+                    } else {
+                        manager.openCamera(cameraId, mCameraDeviceCallback, null);
+                    }
+
+                    return;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createCaptureSession() {
+        if (!mTextureView.isAvailable()) {
+            return;
+        }
+
+        SurfaceTexture texture = mTextureView.getSurfaceTexture();
+        texture.setDefaultBufferSize(mCameraSize.getWidth(), mCameraSize.getHeight());
+        Surface surface = new Surface(texture);
+        try {
+            mPreviewBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        mPreviewBuilder.addTarget(surface);
+        try {
+            mCamera.createCaptureSession(Collections.singletonList(surface), mCameraCaptureSessionCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePreview() {
+        mPreviewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+        HandlerThread thread = new HandlerThread("CameraPreview");
+        thread.start();
+        Handler backgroundHandler = new Handler(thread.getLooper());
+
+        try {
+            mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions((Activity) mContext, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA_PERMISSION);
+    }
+
+    public Bitmap capture() {
+        Log.d("Camera#capture", "start capture");
+        Bitmap bitmap;
+        try {
+            mPreviewSession.stopRepeating();
+            if (mTextureView.isAvailable()) {
+                bitmap = mTextureView.getBitmap();
+                Log.d("Camera#capture", "success");
+                return bitmap;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
